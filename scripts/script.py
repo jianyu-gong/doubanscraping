5291b336a095725b8523cfbbf1755a617379bbd9
Jianyu Gong <jianyu0503@gmail.com>
2021-02-11 01:06:35 -0500

    整合爬取一个小组中的所有成员

--- scripts/script.py
+++ scripts/script.py
@@ -1,13 +1,19 @@
 import requests
+import pandas as pd
 
-
-def readWebData(page, topicNum, cookie):
+def readWebData(page, entityNum, cookie, info):
     header={
     "Cookie": cookie,
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63"
     }
-    url = "https://www.douban.com/group/topic/"+ topicNum + "/?start=" + str(page)
+    if info == "comment":
+        url = "https://www.douban.com/group/topic/"+ entityNum + "/?start=" + str(page)
+    elif info == "member":
+        url = "https://www.douban.com/group/"+ entityNum + "/members?start=" + str(page)
+
     r = requests.get(url=url,headers=header)
+    print('正在处理: %s' % url)
+    statusCode(r)
     data = r.content.decode('utf-8')
     return data
 
@@ -25,3 +31,35 @@ def createCommentInfoList(webInfo, topicNum, replyIdList, replyIdAltList, commen
         timeList.append(timestamp)
         topicNumList.append(topicNum)
     return replyIdList, replyIdAltList, commentList, timeList, topicNumList
+
+
+def statusCode(response):
+    if response.status_code == 200:
+        print('抓取成功!')
+    elif response.status_code == 404:
+        print('抓取失败.')
+
+
+def createMemInfoList(memberInfo, memberIdUrlList):
+    for member in memberInfo:
+        memberIdAlt = member.find("a", class_='').get_text()
+        memberIdUrl = member.find("a", class_='')['href']
+        print('Member Name: %s' % memberIdAlt)
+        print('Member URL: %s' % memberIdUrl)
+
+        memberIdUrlList.append(memberIdUrl)
+
+    return memberIdUrlList
+
+
+def processUserId(memberIdUrlList):
+    memberId = []
+    for url in memberIdUrlList:
+        memberId.append(url[30:-1])
+    dataframe = pd.DataFrame(list(zip(memberId)), 
+                columns =['UserID'])
+
+    dataframe = dataframe.drop_duplicates()
+    recordCount = len(dataframe)
+    print("一共有%d个用户关注这个小组。" % recordCount)
+    return dataframe
