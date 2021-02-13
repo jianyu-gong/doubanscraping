5501576c53a77d56b6e54db9a013364dda0252b6
Jianyu Gong <jianyu0503@gmail.com>
2021-02-13 16:54:07 -0500

    使用configparser来隐藏个人信息

--- main.py
+++ main.py
@@ -1,23 +1,58 @@
-import requests
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
-from scripts.script import readWebData, createCommentInfoList, createMemInfoList, processUserId
+from scripts.script import readWebData, createCommentInfoList, createMemInfoList
 from time import sleep
+from mysql.connector import errorcode
+import mysql.connector
+import configparser
 
+config = configparser.ConfigParser()
+config.read("config/config.txt")
 
-cookie =  """bid=M8d5os6pdMc; __utmc=30149280; __utmz=30149280.1612629628.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not provided); push_doumail_num=0; __utmv=30149280.23180; ct=y; douban-profile-remind=1; ll="108120"; dbcl2="231803702:BHiciIvzZnI"; ck=q-BM; __yadk_uid=ymcyjW0WJCF8VEIKnvmnB80XfOSZfLjZ; push_noty_num=0; ap_v=0,6.0; _pk_ref.100001.8cb4=["","",1612995689,"https://www.google.com/"]; _pk_ses.100001.8cb4=*; __utma=30149280.40126775.1612629628.1612986153.1612995690.15; __utmt=1; _pk_id.100001.8cb4=a851698bf89c60d1.1612629627.15.1613000006.1612986711.; __utmb=30149280.103.5.1613000003662"""
+host = config.get("configuration","host")
+cookie = config.get("configuration","cookie")
+user = config.get("configuration","user")
+password = config.get("configuration","password")
+database = config.get("configuration","database")
 
-memberIdAltList = []
-memberIdUrlList = []
-entityNum = '634189'
+entityNum = "634189"
+
+# Obtain connection string information from the portal
+config = {
+'host': host,
+'user': user,
+'password': password,
+'database': database,
+'ssl_ca': 'config/DigiCertGlobalRootG2.crt.pem',
+'charset': 'utf8mb4'
+}
+
+# Construct connection string
+try:
+    conn = mysql.connector.connect(**config)
+    print("Connection established")
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with the user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cursor = conn.cursor()
 
 for page in range(0, 1000, 35):
     data = readWebData(page, entityNum, cookie, info="member")
     soup = BeautifulSoup(data, 'html.parser')
     memberInfo = soup.find_all('div', class_='name')
-    memberIdUrlList = createMemInfoList(memberInfo, memberIdUrlList)
+    if len(memberInfo) == 0:
+        print("返回零个记录，需要登录验证。")
+        break
+    createMemInfoList(memberInfo, cursor, conn)
     sleep(2.0)
-
-memberDf = processUserId(memberIdUrlList)
-memberDf.to_csv('人才组用户名单.csv', index=False)
+    
+print(cursor.rowcount, "records inserted.")
+conn.commit()
+cursor.close()
+conn.close()
