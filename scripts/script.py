9180b4c12cf564365e430eca17c912932aa8c9bb
Jianyu Gong <jianyu0503@gmail.com>
2021-02-15 17:50:57 -0500

    加入将一个贴子的全部评论导入数据库

--- scripts/script.py
+++ scripts/script.py
@@ -20,19 +20,19 @@ def readWebData(page, entityNum, cookie, info):
     return data
 
 
-def createCommentInfoList(webInfo, topicNum, replyIdList, replyIdAltList, commentList, timeList):
-    topicNumList = []
+def createCommentInfoList(webInfo, topicNum, cursor):
     for reply in webInfo:
         replyId = reply["data-author-id"]
         replyIdAlt = list(reply.find("h4"))[1].get_text()
         comment = reply.find("p", class_='reply-content').get_text()
         timestamp = reply.find("span", class_='pubtime').get_text()
-        replyIdList.append(replyId)
-        replyIdAltList.append(replyIdAlt)
-        commentList.append(comment)
-        timeList.append(timestamp)
-        topicNumList.append(topicNum)
-    return replyIdList, replyIdAltList, commentList, timeList, topicNumList
+
+        print('Member Name: %s' % replyIdAlt)
+        print('Member Comment: %s' % comment)
+
+        sql = "INSERT IGNORE INTO comment (userID, commentTxt, topicNum, commentTime) VALUES (%s, %s, %s, %s)"
+        val = (replyId, comment, topicNum, timestamp)
+        cursor.execute(sql, val)
 
 
 def statusCode(response):
@@ -42,15 +42,15 @@ def statusCode(response):
         print('抓取失败.')
 
 
-def createMemInfoList(memberInfo, cursor, conn):
+def createMemInfoList(memberInfo, cursor, conn, entityNum):
     for member in memberInfo:
         memberIdAlt = member.find("a", class_='').get_text()
         memberIdUrl = member.find("a", class_='')['href']
         print('Member Name: %s' % memberIdAlt)
         print('Member URL: %s' % memberIdUrl)
 
-        sql = "INSERT IGNORE INTO user (userID, userAlt) VALUES (%s, %s)"
-        val = (memberIdUrl[30:-1], memberIdAlt)
+        sql = "INSERT IGNORE INTO userGroup (userID, userAlt, groupNum) VALUES (%s, %s, %s)"
+        val = (memberIdUrl[30:-1], memberIdAlt, entityNum)
         cursor.execute(sql, val)
 
 
