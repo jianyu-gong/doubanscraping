125947d827b00f9fef838f5966e57d728b3409fb
Jianyu Gong <jianyu0503@gmail.com>
2021-02-15 22:47:43 -0500

    搭建数据管道实现从一个小组调取所有人然后调取这些用户关注的小组并调取这些小组的标签

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
-from scripts.script import readWebData, createCommentInfoList, createMemInfoList
+from scripts.script import readWebData, createCommentInfoList, createMemInfoList, createMemberJoinedGroup, createTagInfo
 from time import sleep
 from mysql.connector import errorcode
 import mysql.connector
@@ -16,7 +16,7 @@ user = config.get("configuration","user")
 password = config.get("configuration","password")
 database = config.get("configuration","database")
 
-entityNum = "634189"
+topicNum = "211190218"
 
 # Obtain connection string information from the portal
 config = {
@@ -42,17 +42,70 @@ except mysql.connector.Error as err:
 else:
     cursor = conn.cursor()
 
-for page in range(0, 1000, 35):
-    data = readWebData(page, entityNum, cookie, info="member")
+# 找到一个小组里的所有成员
+# entityNum = "634189"
+# for page in range(0, 1000, 35):
+#     data = readWebData(page, entityNum, cookie, info="member")
+#     soup = BeautifulSoup(data, 'html.parser')
+#     memberInfo = soup.find_all('div', class_='name')
+#     if len(memberInfo) == 0:
+#         print("返回零个记录，需要登录验证。")
+#         break
+#     createMemInfoList(memberInfo, cursor, entityNum)
+#     sleep(2.0)
+
+# 找到一个贴子下所有评论
+# commentCount = 1
+# page = 0
+# while commentCount != 0:
+#     data = readWebData(page, topicNum, cookie, info="comment")
+#     soup = BeautifulSoup(data, 'html.parser')
+#     webInfo = soup.find_all('li', class_='clearfix comment-item reply-item')
+#     if len(webInfo) == 0:
+#         print("返回零个记录，需要登录验证。")
+#         break
+#     createCommentInfoList(webInfo, topicNum, cursor)
+#     commentCount = len(soup.find_all('li', class_='clearfix comment-item reply-item'))
+#     page+=100
+#     sleep(2.0)
+
+
+# query = "SELECT DISTINCT userID FROM userGroup"
+# cursor.execute(query)
+# result = list(cursor.fetchall())
+# userId = []
+# for n in result:
+#     userId.append(n[0].decode())
+# print('一共有%d个组员需要处理' % len(userId))
+# sleep(5.0)
+
+# for user_id in userId:
+#     data = readWebData(0, user_id, cookie, info="joinedgroup")
+#     soup = BeautifulSoup(data, 'html.parser')
+#     joinedGroupInfo = soup.find_all('div', class_='title')
+#     createMemberJoinedGroup(joinedGroupInfo, cursor, user_id)
+#     sleep(1.0)
+
+query = "SELECT DISTINCT groupID FROM relation"
+cursor.execute(query)
+result = list(cursor.fetchall())
+groupID = []
+for n in result:
+    groupID.append(n[0].decode())
+print('一共有%d个小组需要处理' % len(groupID))
+sleep(5.0)
+
+i = 1
+for group in groupID:
+    print('正在处理第%d个小组...' % i)
+    data = readWebData(0, group, cookie, "group")
     soup = BeautifulSoup(data, 'html.parser')
-    memberInfo = soup.find_all('div', class_='name')
-    if len(memberInfo) == 0:
-        print("返回零个记录，需要登录验证。")
-        break
-    createMemInfoList(memberInfo, cursor, conn)
-    sleep(2.0)
-    
-print(cursor.rowcount, "records inserted.")
+    groupInfo = soup.find('div', class_='group-board')
+    createTagInfo(groupInfo, cursor, group)
+    i += 1
+    sleep(1.0)
+
+#print(cursor.rowcount, "records inserted.")
 conn.commit()
 cursor.close()
 conn.close()
--- scripts/script.py
+++ scripts/script.py
@@ -12,6 +12,10 @@ def readWebData(page, entityNum, cookie, info):
         url = "https://www.douban.com/group/topic/"+ entityNum + "/?start=" + str(page)
     elif info == "member":
         url = "https://www.douban.com/group/"+ entityNum + "/members?start=" + str(page)
+    elif info == "joinedgroup":
+        url = "https://www.douban.com/group/people/" + entityNum + "/joins"
+    elif info == "group":
+        url = "https://www.douban.com/group/" + entityNum + "/"
 
     r = requests.get(url=url,headers=header)
     print('正在处理: %s' % url)
@@ -42,7 +46,7 @@ def statusCode(response):
         print('抓取失败.')
 
 
-def createMemInfoList(memberInfo, cursor, conn, entityNum):
+def createMemInfoList(memberInfo, cursor, entityNum):
     for member in memberInfo:
         memberIdAlt = member.find("a", class_='').get_text()
         memberIdUrl = member.find("a", class_='')['href']
@@ -51,6 +55,34 @@ def createMemInfoList(memberInfo, cursor, conn, entityNum):
 
         sql = "INSERT IGNORE INTO userGroup (userID, userAlt, groupNum) VALUES (%s, %s, %s)"
         val = (memberIdUrl[30:-1], memberIdAlt, entityNum)
+        cursor.execute(sql, val)
+
+
+def createMemberJoinedGroup(joinedGroupInfo, cursor, entityNum):
+    for info in joinedGroupInfo:
+        groupName = info.find('a').get_text()
+        groupID = info.find('a')['href'][29:-1]
+        print('小组ID: %s' % groupID)
+        print('小组名称: %s' % groupName)
+
+        sql = "INSERT IGNORE INTO relation (groupID, groupName, userID) VALUES (%s, %s, %s)"
+        val = (groupID, groupName, entityNum)
+        cursor.execute(sql, val)
+
+
+def createTagInfo(groupInfo, cursor, entityNum):
+    try:
+        createDate = groupInfo.find("p").get_text()[8:18]
+    except:
+        createDate = None
+    for n in range(0,len(groupInfo.find_all('a', class_='tag'))):
+        tag = groupInfo.find_all('a', class_='tag')[n].get_text()
+        print('小组ID: %s' % entityNum)
+        print('创建日期: %s' % createDate)
+        print('标签: %s' % tag)
+
+        sql = "INSERT IGNORE INTO groupinfo (groupID, createDate, tag) VALUES (%s, %s, %s)"
+        val = (entityNum, createDate, tag)
         cursor.execute(sql, val)
 
 
